4aa1489d0dde0bb9640e309a0afdd9b56e4d1f22
Sharad3333 <142505466+Sharad3333@users.noreply.github.com>
2023-08-17 18:35:10 +0530

    Update streamlit_app.py
    added  header & desc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Booled Free-Range Egg')
